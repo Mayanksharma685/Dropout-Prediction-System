cfc70ed5559ab8c5627c9ce644f6f32e9549c6b1
Aditya30ag <adityaagrwal3005@gmail.com>
2025-09-29 17:43:21 +0530

    new

--- qr-backend/config/redisClient.d.ts
+++
@@ -1,4 +0,0 @@
-import type { RedisClientType } from "redis";
-export declare let redis: RedisClientType;
-export declare function connectRedis(): Promise<void>;
-//# sourceMappingURL=redisClient.d.ts.map
